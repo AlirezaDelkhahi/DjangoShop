3c76ca58bbda4a55a7cd930d17b5df6c84d207e4
Alireza <alirezac257@gmail.com>
2022-02-14 18:49:22 +0330

    installing apps and template dir config

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'customer.apps.CustomerConfig',
+    'order.apps.OrderConfig',
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'Shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
