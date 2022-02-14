89cf6d094ed94433994c9aef618cc3dacb2b4694
Alireza <alirezac257@gmail.com>
2022-02-14 21:41:13 +0330

    added media config

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -117,12 +117,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static Config
+STATIC_URL = '/static/'
+
+# Media Config
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
