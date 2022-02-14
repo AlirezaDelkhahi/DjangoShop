bd0104cc701f727572a6c6725d4b6bdabd7eacf0
Alireza <alirezac257@gmail.com>
2022-02-14 22:13:01 +0330

    added Customer Model

--- Shop/customer/models.py
+++ Shop/customer/models.py
@@ -9,3 +9,24 @@ class Address(models.Model):
     postal_code = models.CharField(max_length=100)
     address = models.TextField()
     customer = models.ForeignKey("Customer", on_delete=models.CASCADE, related_name='userAddress')
+
+class Customer(models.Model):
+    
+    gender_choices = (
+        ('male','مذکر'),
+        ('female','مونث')
+        )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, verbose_name='First Name')
+    last_name = models.CharField(max_length=100, verbose_name='Last Name')
+    phone = models.CharField(max_length=100, verbose_name='Phone Number')
+    image = models.FileField(default='Default-Images/default_user,png', upload_to=img_path, blank=True, null=True, verbose_name='Profile Image')
+    gender = models.CharField(choices=gender_choices, null=True, blank=True, verbose_name='Gender')
+
+    @property
+    def img_path(self):
+        """
+            this property creates a dynamic directory name for each user
+        """
+        return f'{self.id}-{self.first_name}_{self.last_name}'
