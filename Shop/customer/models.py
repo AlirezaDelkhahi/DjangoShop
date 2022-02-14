82a021babf6a63b9295c548ed692e4b1519824cd
Alireza <alirezac257@gmail.com>
2022-02-14 21:25:15 +0330

    created Address model

--- Shop/customer/models.py
+++ Shop/customer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Address(models.Model):
+    """
+        Customer's Addresses (a customer can have multiple addresses)
+    """
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=100)
+    address = models.TextField()
+    customer = models.ForeignKey("Customer", on_delete=models.CASCADE, related_name='userAddress')
