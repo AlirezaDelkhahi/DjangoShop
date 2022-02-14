22ce300a81fd3faa5d2c38e4d4c57ac225527883
Alireza <alirezac257@gmail.com>
2022-02-14 23:08:00 +0330

    Created Customer Admin Model

--- Shop/customer/admin.py
+++ Shop/customer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Address
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone')
+    search_fields = ('first_name', 'last_name', 'phone')
+    list_filter = ('last_updated', )
