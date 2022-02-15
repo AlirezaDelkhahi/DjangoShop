4c6503d363578ec37bce86fa39e62e6874014978
Alireza <alirezac257@gmail.com>
2022-02-15 16:17:39 +0330

    [*] Registered Coupon Admin model

--- Shop/order/admin.py
+++ Shop/order/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Coupon
 # Register your models here.
+
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    fields = ['code', 'value', 'type', 'max_price', 'valid_from', 'valid_to', 'is_active']
