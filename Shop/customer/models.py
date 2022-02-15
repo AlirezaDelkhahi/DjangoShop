3cb3c1e4a697848d1df839f3a2c8df325592810e
Alireza <alirezac257@gmail.com>
2022-02-15 13:48:44 +0330

    added str method for customer and address models

--- Shop/customer/models.py
+++ Shop/customer/models.py
@@ -13,6 +13,9 @@ class Address(BaseModel):
     address = models.TextField()
     customer = models.ForeignKey("Customer", on_delete=models.CASCADE, related_name='userAddress')
 
+    def __str__(self):
+        return f'{self.customer} {self.city}'
+
 
 def get_upload_path(instance, filename):
     return os.path.join(
@@ -33,5 +36,8 @@ class Customer(BaseModel):
     first_name = models.CharField(max_length=100, verbose_name='First Name')
     last_name = models.CharField(max_length=100, verbose_name='Last Name')
     phone = models.CharField(max_length=100, verbose_name='Phone Number')
-    image = models.FileField(default='Default-Images/default_user,png', upload_to=get_upload_path, blank=True, null=True, verbose_name='Profile Image')
+    image = models.FileField(default='Default-Images/default_user,png', upload_to='user_profiles/', blank=True, null=True, verbose_name='Profile Image')
     gender = models.CharField(choices=gender_choices, null=True, blank=True, verbose_name='Gender', max_length=10)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
