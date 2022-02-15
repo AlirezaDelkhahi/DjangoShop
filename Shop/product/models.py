13de57b116c7c238cdd04be1609ac8fdbded0c88
Alireza <alirezac257@gmail.com>
2022-02-15 13:47:35 +0330

    added Category model

--- Shop/product/models.py
+++ Shop/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from core.models import BaseModel
+from customer.models import Address
 
-# Create your models here.
+
+class Category(BaseModel):
+    """
+        Category Model has Self Relation
+    """
+    name = models.CharField(max_length=100)
+    thumbnail = models.FileField(default='Default-Images/default_category.png')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
