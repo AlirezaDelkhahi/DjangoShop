e2aaa7a383185850221f57f0c80ea3fbc126e60d
Alireza <alirezac257@gmail.com>
2022-02-15 16:16:49 +0330

    [+] Coupon Model

--- Shop/order/models.py
+++ Shop/order/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from core.models import BaseDiscount
 # Create your models here.
+
+
+class Coupon(BaseDiscount):
+    code = models.CharField(max_length=50)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+
+    def __str__(self):
+        if self.type == 'percent':
+            return f'{self.code}| {self.value}%'
+        return f'{self.code}| {self.value}$'
