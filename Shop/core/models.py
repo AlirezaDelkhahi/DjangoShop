090a130934b4bc502b413e75245877e06b55d6c7
Alireza <alirezac257@gmail.com>
2022-02-15 16:16:35 +0330

    [+] Base Model for Discount

--- Shop/core/models.py
+++ Shop/core/models.py
@@ -46,3 +46,24 @@ class BaseModel(models.Model):
     def activate(self):
         self.is_active = True
         self.save()
+
+
+class BaseDiscount(BaseModel):
+    value = models.PositiveIntegerField(null=False)
+    type = models.CharField(max_length=10, choices=[('price', 'Price'), ('percent', 'Percent')], null=False)
+    max_price = models.PositiveIntegerField(null=True, blank=True)
+
+    def profit_value(self, price: int):
+        """
+        Calculate and Return the profit of the discount
+        :param price: int (item value)
+        :return: profit
+        """
+        if self.type == 'price':
+            return min(self.value, price)
+        else:  # percent
+            raw_profit = int((self.value / 100) * price)
+            return int(min(raw_profit, int(self.max_price))) if self.max_price else raw_profit
+
+    class Meta:
+        abstract = True
