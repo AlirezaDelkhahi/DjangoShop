6bb604b50ed8f240bc3ae76fc07573edd4c0babd
Alireza <alirezac257@gmail.com>
2022-02-15 16:40:33 +0330

    [+] Product Model

--- Shop/product/models.py
+++ Shop/product/models.py
@@ -10,7 +10,7 @@ class Category(BaseModel):
     """
     name = models.CharField(max_length=100)
     thumbnail = models.FileField(default='Default-Images/default_category.png')
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children')
 
     def __str__(self):
         return f'{self.name}'
@@ -34,7 +34,8 @@ class Brand(BaseModel):
     """
     name = models.CharField(max_length=100, verbose_name='Brand Name')
     country = models.CharField(max_length=100, verbose_name='Brand Country')
-    provider = models.ForeignKey(Provider, on_delete=models.SET_NULL, verbose_name='Brand Provider', null=True, blank=True)
+    provider = models.ForeignKey(Provider, on_delete=models.SET_NULL, verbose_name='Brand Provider',
+                                 null=True, blank=True)
     description = models.TextField(null=True, blank=True, verbose_name='Brand Details')
 
     def __str__(self):
@@ -51,3 +52,18 @@ class Discount(BaseDiscount):
         if self.type == 'percent':
             return f'{self.value}%'
         return f'{self.value}$'
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100, verbose_name='Name')
+    description = models.TextField(null=True, blank=True, verbose_name='Description')
+    slug = models.SlugField()
+    category = models.ManyToManyField(Category, on_delete=models.SET_NULL,
+                                      verbose_name='Category',
+                                      related_name='products',
+                                      null=True, blank=True)
+    image = models.FileField(default='Default-Images/product_default.jpg', upload_to='Products/', null=True, blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Brand',
+                              related_name='products')
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True, blank=True,
+                                 verbose_name='Discount', related_name='products')
