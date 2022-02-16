0c455e8fffbddd16075275a85d44fe66e0623c21
Alireza <alirezac257@gmail.com>
2022-02-16 13:52:05 +0330

    [*] fixed bugs

--- Shop/order/admin.py
+++ Shop/order/admin.py
@@ -7,18 +7,18 @@ from .models import Coupon, Cart, CartItem
 class CouponAdmin(admin.ModelAdmin):
     list_display = ['code', 'value', 'type', 'max_price', 'valid_from', 'valid_to', 'is_active']
     search_fields = ('code',)
-    list_filter = 'last_updated'
+    list_filter = ['last_updated']
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ['customer', 'discount', 'address']
     search_fields = ('customer', 'address')
-    list_filter = 'last_updated'
+    list_filter = ['last_updated']
 
 
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ['product', 'quantity']
     search_fields = ('product', )
-    list_filter = 'last_updated'
+    list_filter = ['last_updated']
