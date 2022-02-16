658b18f7cdafaee5cdcdb5651c3a70e09c5eeeab
Alireza <alirezac257@gmail.com>
2022-02-16 11:13:51 +0330

    [+] Final Price for cart items

--- Shop/order/models.py
+++ Shop/order/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from core.models import BaseDiscount, BaseModel
+from customer.models import Customer
 from product.models import Product
-# Create your models here.
 
 
 class Coupon(BaseDiscount):
@@ -18,7 +18,19 @@ class Coupon(BaseDiscount):
 class CartItem(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Item')
     quantity = models.IntegerField(default=1, verbose_name='Quantity')
-    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='items')
+
+    @property
+    def final_price(self):
+        """
+         Calculate and Return the Final Price of an order item
+        :param: instance
+        :return: final_price
+        """
+        if self.product.discount:
+            return self.product.price - self.product.discount.profit_value(self.product.price) * self.quantity
+        return self.product.price * self.quantity
 
     def __str__(self):
         return f'{self.product.name}: {self.quantity}x'
+
