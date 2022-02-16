798a462caba62f7127e5e48f1b042bbaeb57a668
Alireza <alirezac257@gmail.com>
2022-02-16 11:59:07 +0330

    [+] Cart Model

--- Shop/order/models.py
+++ Shop/order/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from core.models import BaseDiscount, BaseModel
 from customer.models import Customer
 from product.models import Product
@@ -34,3 +35,42 @@ class CartItem(BaseModel):
     def __str__(self):
         return f'{self.product.name}: {self.quantity}x'
 
+
+class Cart(BaseModel):
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE,
+                                    verbose_name='Customer', related_name='customer',
+                                    null=True, blank=True)
+    discount = models.IntegerField(verbose_name='Coupon', blank=True, null=True, default=0)
+    address = models.TextField(verbose_name='Shipping Address', null=True, blank=True)
+
+    @property
+    def total_price(self):
+        """
+            Calculate and return total_price of Cart (without Coupon)
+        """
+        return sum([x.final_price for x in self.items.all()])
+
+    @property
+    def final_price(self):
+        """
+            Calculate and return final_price of Cart (with Coupon)
+            :param: self
+            :return: final_price
+        """
+        total_price = sum([x.final_price for x in self.items.all()])
+        if self.discount:
+            return total_price - self.discount
+        return total_price
+
+    def add_coupon(self, coupon: Coupon):
+        """
+            validates and calculates profit of a coupon and gives it to cart's discount field
+            :param: coupon instance
+            :return: profit discount (if coupon is  valid)
+        """
+        if coupon.is_active and (coupon.valid_from <= timezone.now() <= coupon.valid_to):
+            self.discount = coupon.profit_value(self.final_price)
+        return self.discount
+
+    def __str__(self):
+        return f'{self.customer} - {self.final_price}'
