34046f3a167c991e0eb57487fe4669467841a110
Alireza <alirezac257@gmail.com>
2022-02-16 12:40:01 +0330

    [+] Registered Order App's Models

--- Shop/order/admin.py
+++ Shop/order/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from .models import Coupon
+from .models import Coupon, Cart, CartItem
 # Register your models here.
 
 
 @admin.register(Coupon)
 class CouponAdmin(admin.ModelAdmin):
-    fields = ['code', 'value', 'type', 'max_price', 'valid_from', 'valid_to', 'is_active']
+    list_display = ['code', 'value', 'type', 'max_price', 'valid_from', 'valid_to', 'is_active']
+    search_fields = ('code',)
+    list_filter = 'last_updated'
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'discount', 'address']
+    search_fields = ('customer', 'address')
+    list_filter = 'last_updated'
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'quantity']
+    search_fields = ('product', )
+    list_filter = 'last_updated'
