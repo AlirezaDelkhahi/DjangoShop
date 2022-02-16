f06abe71a973ba239e06fdd20189cadd1f268285
Alireza <alirezac257@gmail.com>
2022-02-16 17:37:11 +0330

    [+] test url for home page

--- Shop/Shop/urls.py
+++ Shop/Shop/urls.py
@@ -3,7 +3,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from product.views import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', test),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
