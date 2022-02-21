49fcb0206f06d445ad05cabb1fdf821dbfe2a3f0
Alireza <alirezac257@gmail.com>
2022-02-21 16:17:10 +0330

    [*] refactor customer admin

--- Shop/customer/admin.py
+++ Shop/customer/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Customer, Address
 
+from core.models import User
+
+
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone')
-    search_fields = ('first_name', 'last_name', 'phone')
+    list_display = ['user']
+    fields = ['user', 'gender', 'image']
     list_filter = ('last_updated', )
