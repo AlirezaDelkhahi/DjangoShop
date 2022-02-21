1fc14d5aa46f1c40d2259790fa6c456005f5b218
Alireza <alirezac257@gmail.com>
2022-02-21 16:16:07 +0330

    [*] customize user admin

--- Shop/core/admin.py
+++ Shop/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+UserAdmin.list_display =('phone', 'email', 'first_name', 'last_name', 'is_staff')
+UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
+UserAdmin.add_fieldsets[0][1]['fields'] = ('phone', 'password1', 'password2')
+admin.site.register(User, UserAdmin)
