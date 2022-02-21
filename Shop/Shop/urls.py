eb4e002f52b40c20440760f4b7ce6e1245a54904
Alireza <alirezac257@gmail.com>
2022-02-22 02:37:32 +0330

    [+] setting product app's urls as root path

--- Shop/Shop/urls.py
+++ Shop/Shop/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path(''),
     path('customer', include('customer.urls', namespace='customer')),
-    path('product', include('product.urls', namespace='product')),
+    path('', include('product.urls', namespace='product')),
     path('order', include('order.urls', namespace='order')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
