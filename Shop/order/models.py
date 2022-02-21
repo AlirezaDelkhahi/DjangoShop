e592b20ad0cabaaeb374dc2c18f6d7db7ab8fb3c
Alireza <alirezac257@gmail.com>
2022-02-21 16:57:07 +0330

    [*] final and total properties added to model fields and refactor methods

--- Shop/order/models.py
+++ Shop/order/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from core.models import BaseDiscount, BaseModel
-from customer.models import Customer
+from customer.models import Customer, Address
 from product.models import Product
-
 
 class Coupon(BaseDiscount):
     code = models.CharField(max_length=50)
@@ -20,52 +19,50 @@ class CartItem(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Item')
     quantity = models.IntegerField(default=1, verbose_name='Quantity')
     cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='items', null=True, blank=True)
-
-    @property
-    def final_price(self):
+    final_price = models.IntegerField(default=0, null=True, blank=True)
+    
+    def calc_final_price(self):
         """
-         Calculate and Return the Final Price of an order item
+        Calculate and Return the Final Price of an order item
         :param: instance
-        :return: final_price
+        :return final_price:
         """
-        if self.product.discount:
-            return (self.product.price - self.product.discount.profit_value(self.product.price)) * self.quantity
-        return self.product.price * self.quantity
+        self.final_price =  (self.product.price - self.product.discount.profit_value(self.product.price)) * self.quantity if self.product.discount else self.product.price * self.quantity
+        return self.final_price
+        
 
     def __str__(self):
         return f'{self.product.name}: {self.quantity}x'
 
 
 class Cart(BaseModel):
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE,
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE,
                                     verbose_name='Customer', related_name='customer',
                                     null=True, blank=True)
-    discount = models.IntegerField(verbose_name='Coupon', blank=True, null=True, default=0)
-    address = models.TextField(verbose_name='Shipping Address', null=True, blank=True)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, blank=True, null=True)
+    coupon = models.ForeignKey(Coupon, on_delete=models.SET_NULL, null=True, blank=True)
+    final_price = models.IntegerField(default=0, null=True, blank=True)
+    total_price = models.IntegerField(default=0, null=True, blank=True)
 
-    # TODO: total_price Property
+    def calc_total_price(self):
+        """
+            Calculate and return total_price of Cart
+            :param: self
+            :return total_price:
+        """
+        self.total_price = sum([x.final_price for x in self.items.all()])
+        return self.total_price
 
-    @property
-    def final_price(self):
+
+    def calc_final_price(self):
         """
             Calculate and return final_price of Cart (with Coupon)
             :param: self
-            :return: final_price
+            :return final_price:
         """
-        total_price = sum([x.final_price for x in self.items.all()])
-        if self.discount:
-            return total_price - self.discount
-        return total_price
+        self.final_price = (self.total_price - self.coupon.profit_value(self.final_price)) if self.coupon else self.total_price
+        return self.final_price
 
-    def add_coupon(self, coupon: Coupon):
-        """
-            validates and calculates profit of a coupon and gives it to cart's discount field
-            :param: coupon instance
-            :return: profit discount (if coupon is  valid)
-        """
-        if coupon.is_active and (coupon.valid_from <= timezone.now() <= coupon.valid_to):
-            self.discount = coupon.profit_value(self.final_price)
-        return self.discount
 
     def __str__(self):
         return f'{self.customer} - {self.final_price}'
