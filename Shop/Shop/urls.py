8dad4ab6644941e1cc81f2d9120d35584d9fc448
Alireza <alirezac257@gmail.com>
2022-02-21 20:44:48 +0330

    [+] Shop Urls

--- Shop/Shop/urls.py
+++ Shop/Shop/urls.py
@@ -2,11 +2,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from product.views import test
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', test),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # path(''),
+    path('customer', include('customer.urls', namespace='customer')),
+    path('product', include('product.urls', namespace='product')),
+    path('order', include('order.urls', namespace='order')),
 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
