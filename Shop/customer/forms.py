3006bd885f8487a81f5b053b3112e47248cebfd9
Alireza <alirezac257@gmail.com>
2022-02-21 20:44:13 +0330

    [+] Form Validators for User Registration form

--- Shop/customer/forms.py
+++ Shop/customer/forms.py
@@ -14,3 +14,32 @@ class UserRegistrationForm(forms.Form):
     gender = forms.ChoiceField(choices=gender_choices, widget=forms.Select(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        if User.objects.filter(phone=phone).exists():
+            raise ValidationError('This Phone Number already exists !')
+        else:
+            return phone
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise ValidationError('This Username already exists !')
+        else:
+            return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(username=email).exists():
+            raise ValidationError('This Email already exists !')
+        else:
+            return email
+
+    def clean(self):
+        clean_data = super().clean()
+        password = clean_data.get('password1')
+        confirm = clean_data.get('password2')
+
+        if password and confirm and password != confirm:
+            raise ValidationError('Password is not match !')
