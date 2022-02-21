c6c052aa4d95637261420c9f5320d2eaa37cd09b
Alireza <alirezac257@gmail.com>
2022-02-22 03:12:56 +0330

    [+] User Registration View

--- Shop/customer/views.py
+++ Shop/customer/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserRegistrationForm
 # Create your views here.
+
+
+class UserRegisterView(View):
+    template_name = 'customer/register.html'
+    form = UserRegistrationForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('product:home')
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, self.template_name, {'form': form})
+
