6d85605d5bbafe2b0a2d5e75eb4a4b9d58fd29c3
Alireza <alirezac257@gmail.com>
2022-02-22 16:47:36 +0330

    [+] Reset Password Url Paths

--- Shop/customer/urls.py
+++ Shop/customer/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from .views import UserRegisterView, UserLoginView, UserLogoutView
+from .views import UserRegisterView, UserLoginView, UserLogoutView, UserPasswordResetView, UserPasswordResetDoneView, \
+    UserPasswordResetConfirmView, UserPasswordResetCompleteView
 
 app_name = 'customer'
 
@@ -7,4 +8,8 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user-register'),
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('logout/', UserLogoutView.as_view(), name='user-logout'),
+    path('reset/', UserPasswordResetView.as_view(), name='reset_password'),
+    path('reset/done/', UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('confirm/<uidb64>/<token>', UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/complete', UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
