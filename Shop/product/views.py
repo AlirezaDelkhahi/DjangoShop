fcc98d943bc771452ca2938ac2da8a1dfb19e35a
Alireza <alirezac257@gmail.com>
2022-02-22 10:19:45 +0330

    [*] fixing url redirect reference

--- Shop/product/views.py
+++ Shop/product/views.py
@@ -79,7 +79,7 @@ class DetailBrand(View):
         """
         brand: Brand = Brand.objects.get(id=kwargs['brand_id'])
         if not brand.products.all():  # category products is empty
-            return redirect('product:home')
+            return redirect('product:brand-list')
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, brand_id):
