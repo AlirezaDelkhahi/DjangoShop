79b351e6d7cb5fec534f9d0b795c41182d1d1f38
Alireza <alirezac257@gmail.com>
2022-02-22 03:50:16 +0330

    [+] fixed some bugs in user manager

--- Shop/core/models.py
+++ Shop/core/models.py
@@ -70,14 +70,14 @@ class BaseDiscount(BaseModel):
 
 
 class MyUserManager(UserManager):
-    
+
     def create_superuser(self, username=None, email=None, password=None, **extra_fields):
         username = extra_fields['phone']
         return super().create_superuser(username, email, password, **extra_fields)
 
     def create_user(self, username=None, **extra_fields):
         username = extra_fields['phone']
-        return super().create_user(username)
+        return super().create_user(username, **extra_fields)
 
 class User(AbstractUser):
     phone = models.CharField(max_length=15, unique=True)
