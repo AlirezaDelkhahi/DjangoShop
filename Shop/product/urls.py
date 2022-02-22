c50678676e1b1ff80081e3426a5afdf8a368c284
Alireza <alirezac257@gmail.com>
2022-02-22 09:40:44 +0330

    [+] brand product list (brand detail) url path

--- Shop/product/urls.py
+++ Shop/product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, DetailCategory, DetailProduct, ListBrand
+from .views import index, DetailCategory, DetailProduct, ListBrand, DetailBrand
 app_name = 'product'
 
 urlpatterns = [
     path('', index, name='home'),
     path('products/<int:product_id>', DetailProduct.as_view(), name='product-detail'),
     path('categories/<int:category_id>', DetailCategory.as_view(), name='category-detail'),
-    path('brands/', ListBrand.as_view(), name='brand-list')
+    path('brands/', ListBrand.as_view(), name='brand-list'),
+    path('brands/<int:brand_id>', DetailBrand.as_view(), name='brand-detail'),
 ]
