4392cf451dc7cd488f6c7cefee7df8251a9b2293
Alireza <alirezac257@gmail.com>
2022-02-22 07:32:56 +0330

    [+] User Logout url path

--- Shop/customer/urls.py
+++ Shop/customer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import UserRegisterView, UserLoginView
+from .views import UserRegisterView, UserLoginView, UserLogoutView
+
 app_name = 'customer'
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user-register'),
     path('login/', UserLoginView.as_view(), name='user-login'),
+    path('logout/', UserLogoutView.as_view(), name='user-logout'),
 ]
