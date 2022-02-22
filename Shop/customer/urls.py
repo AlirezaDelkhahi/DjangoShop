7ab71f4d30e8ec505f289520e024ab54323fc108
Alireza <alirezac257@gmail.com>
2022-02-22 04:35:41 +0330

    [+] User Login url Path

--- Shop/customer/urls.py
+++ Shop/customer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import UserRegisterView
+from .views import UserRegisterView, UserLoginView
 app_name = 'customer'
 
 urlpatterns = [
-    path('register', UserRegisterView.as_view(), name='user-register')
+    path('register/', UserRegisterView.as_view(), name='user-register'),
+    path('login/', UserLoginView.as_view(), name='user-login'),
 ]
