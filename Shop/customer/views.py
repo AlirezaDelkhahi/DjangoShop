672eed9fe103a4f3dba5ff3cf663f2e78133bc74
Alireza <alirezac257@gmail.com>
2022-02-22 07:58:33 +0330

    [+] some documentation for all views

--- Shop/customer/views.py
+++ Shop/customer/views.py
@@ -10,52 +10,73 @@ from django.contrib import messages
 
 
 class UserRegisterView(View):
+    """
+        View for User Registration
+    """
     template_name = 'customer/register.html'
     form = UserRegistrationForm
 
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
+        """
+            checks if user is already logged in or not
+        """
+        if request.user.is_authenticated:  # a user is already logged in
             return redirect('product:home')
         else:
             return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
+        """
+            sends register template to customer
+        """
         form = UserRegistrationForm
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
+        """
+            gets Registration form data and validates
+        """
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             User.objects.create_user(phone=cd['phone'], email=cd['email'], password=cd['password1'])
             messages.success(request, 'you registered successfully', 'success')
             return redirect('product:home')
-        else:
-            print(form.errors)
-            messages.error(request, 'ridi', 'danger')
-
+        else:  # user sends wrong data in register form
             return render(request, self.template_name, {'form': form})
 
 
 class UserLoginView(View):
+    """
+        View for User login
+    """
     template_name = 'customer/login.html'
     form = UserLoginForm
 
     def dispatch(self, request, *args, **kwargs):
+        """
+            checks if user is already logged in or not
+        """
         if request.user.is_authenticated:
             return redirect('product:home')
         else:
             return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
+        """
+            sends login template to customer
+        """
         return render(request, self.template_name, {'form': self.form()})
 
     def post(self, request):
+        """
+            gets Login form data and validates
+        """
         form = self.form(request.POST)
         if form.is_valid():
             clean_data = form.cleaned_data
             user = authenticate(request, username=clean_data['phone'], password=clean_data['password1'])
-            if user is not None:
+            if user is not None:  # user exists and passed of authentication
                 login(request, user)
                 messages.success(request, 'you logged in successfully', 'success')
                 return redirect('product:home')
@@ -65,6 +86,9 @@ class UserLoginView(View):
 
 
 class UserLogoutView(LoginRequiredMixin, View):
+    """
+        View for User Logout
+    """
     def get(self, request):
         logout(request)
         messages.success(request, "you've been logged out successfully", 'success')
