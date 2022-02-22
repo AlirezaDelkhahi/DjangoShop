4517070bde611815db7a51901b3e389414ba2b45
Alireza <alirezac257@gmail.com>
2022-02-22 10:18:04 +0330

    [*] refactor

--- Shop/product/admin.py
+++ Shop/product/admin.py
@@ -6,21 +6,24 @@ from .models import Product, Category, Discount, Brand, Provider
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price']
-    search_fields = ('name', 'slug', 'category', 'price')
+    fields = ['name', 'description', 'slug', 'category', 'price', 'image', 'brand', 'discount']
+    search_fields = ('name',)
     list_filter = ['last_updated']
-    prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('description',)}
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
+    fields = ['name', 'thumbnail', 'parent']
     search_fields = ('name',)
     list_filter = ['last_updated']
 
 
 @admin.register(Discount)
-class CouponAdmin(admin.ModelAdmin):
-    list_display = ['value', 'type', 'is_active']
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ['value', 'type', 'max_price', 'is_active']
+    fields = ['value', 'type', 'max_price', 'expire_date', 'is_active']
     search_fields = ('value', 'type',)
     list_filter = ['last_updated']
 
@@ -28,12 +31,14 @@ class CouponAdmin(admin.ModelAdmin):
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = ['name', 'country', 'provider']
+    fields = ['name', 'country', 'provider', 'thumbnail', 'description']
     search_fields = ('name', 'provider', 'country')
     list_filter = ['last_updated']
 
 
 @admin.register(Provider)
 class ProviderAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ['name',]
+    fields = ['name', 'description', 'address']
     search_fields = ('name', )
     list_filter = ['last_updated']
