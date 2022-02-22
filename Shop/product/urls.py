e5e74b7c06fc24d97a6dd885c7952a7c340f3075
Alireza <alirezac257@gmail.com>
2022-02-22 07:33:19 +0330

    [+] category detail url path

--- Shop/product/urls.py
+++ Shop/product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, DetailCategory
 app_name = 'product'
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='home'),
+    path('categories/<int:category_id>', DetailCategory.as_view(), name='category-detail')
 ]
