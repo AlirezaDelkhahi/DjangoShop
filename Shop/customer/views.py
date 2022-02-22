b359d648652f7d3746837537a4284fb0090818da
Alireza <alirezac257@gmail.com>
2022-02-22 04:35:48 +0330

    [+] User Login View

--- Shop/customer/views.py
+++ Shop/customer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth import authenticate, login, logout
 from core.models import User
 from django.contrib import messages
 
@@ -33,3 +34,31 @@ class UserRegisterView(View):
             messages.error(request, 'ridi', 'danger')
 
             return render(request, self.template_name, {'form': form})
+
+
+class UserLoginView(View):
+    template_name = 'customer/login.html'
+    form = UserLoginForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('product:home')
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        return render(request, self.template_name, {'form': self.form()})
+
+    def post(self, request):
+        form = self.form(request.POST)
+        if form.is_valid():
+            clean_data = form.cleaned_data
+            user = authenticate(request, username=clean_data['phone'], password=clean_data['password1'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully', 'success')
+                return redirect('product:home')
+            else:
+                messages.error(request, 'username or password is not correct', 'danger')
+        return render(request, self.template_name, {'form': form})
+
