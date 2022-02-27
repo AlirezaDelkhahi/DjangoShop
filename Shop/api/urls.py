158ee244a16456f84a8873a3cb9464587b05013a
Alireza <alirezac257@gmail.com>
2022-02-27 11:54:52 +0330

    [+] customer api url paths

--- Shop/api/urls.py
+++ Shop/api/urls.py
@@ -1,6 +1,6 @@
 # Shop Url configs
 from django.urls import path, include
-from .views import UserListView, AddressDetailView, AddressListView, UserDetailView
+from .views import UserListView, AddressDetailView, AddressListView, UserDetailView, CustomerDetailView, CustomerListView
 
 app_name = 'api'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('address-list', AddressListView.as_view(), name='address-list'),
     path('user-detail/<pk>', UserDetailView.as_view(), name='user-detail'),
     path('address-detail/<pk>', AddressDetailView.as_view(), name='address-detail'),
+    path('customer-detail/<pk>', CustomerDetailView.as_view(), name='customer-detail'),
+    path('customer-list/', CustomerListView.as_view(), name='customer-list'),
 ]
