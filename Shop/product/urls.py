3bc42d037787b3fddbb337e96a14efdf1fd2ecf6
Alireza <alirezac257@gmail.com>
2022-03-01 17:18:39 +0330

    [*] moved some paths

--- Shop/product/urls.py
+++ Shop/product/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import index, DetailCategory, DetailProduct, ListBrand, DetailBrand
+from .views import index, CategoryDetail, ProductDetail, BrandList, BrandDetail, ProductList
 app_name = 'product'
 
 urlpatterns = [
     path('', index, name='home'),
-    path('products/<int:product_id>', DetailProduct.as_view(), name='product-detail'),
-    path('categories/<int:category_id>', DetailCategory.as_view(), name='category-detail'),
-    path('brands/', ListBrand.as_view(), name='brand-list'),
-    path('brands/<int:brand_id>', DetailBrand.as_view(), name='brand-detail'),
+    path('categories/<int:category_id>', CategoryDetail.as_view(), name='category-detail'),
+    path('brands/', BrandList.as_view(), name='brand-list'),
+    path('brands/<int:brand_id>', BrandDetail.as_view(), name='brand-detail'),
+    path('products/<int:product_id>', ProductDetail.as_view(), name='product-detail'),
 ]
