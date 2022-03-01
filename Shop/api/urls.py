1327c7c3d9f935c2681e57c7d9d2c30ba75e94cc
Alireza <alirezac257@gmail.com>
2022-03-01 17:12:47 +0330

    [+] cart api paths

--- Shop/api/urls.py
+++ Shop/api/urls.py
@@ -1,6 +1,6 @@
 # Shop Url configs
 from django.urls import path, include
-from .views import UserListView, AddressDetailView, AddressListView, UserDetailView, CustomerDetailView, CustomerListView
+from .views import UserListView, AddressDetailView, AddressListView, UserDetailView, CustomerDetailView, CustomerListView, CartItemListView, CartItemDetailView, CartListView, CartDetailView
 
 app_name = 'api'
 
@@ -11,4 +11,8 @@ urlpatterns = [
     path('address-detail/<pk>', AddressDetailView.as_view(), name='address-detail'),
     path('customer-detail/<pk>', CustomerDetailView.as_view(), name='customer-detail'),
     path('customer-list/', CustomerListView.as_view(), name='customer-list'),
+    path('cartitem-list/', CartItemListView.as_view(), name='cartitem-list'),
+    path('cartitem-detail/<pk>', CartItemDetailView.as_view(), name='cartitem-detail'),
+    path('cart-list/', CartListView.as_view(), name='cart-list'),
+    path('cart-detail/<pk>', CartDetailView.as_view(), name='cart-detail'),
 ]
