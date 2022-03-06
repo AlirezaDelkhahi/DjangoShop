1511e6f8d1e3f712bf0e10eda3eafc9d3bdf6b00
Alireza <alirezac257@gmail.com>
2022-03-06 15:59:48 +0330

    [+] CartView

--- Shop/order/views.py
+++ Shop/order/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
+from .cart import Cart
+from product.models import Product
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+
+class CartView(View):
+    template_name = 'order/cart.html'
+
+    def setup(self, request, *args, **kwargs):
+        self.cart = Cart(request)
+        return super().setup(request, *args, **kwargs)
+
+    # def get(self, request):
+    #     return render(request, template_name, {"cart": self.cart})
+
+    def post(self,request, product_id, quantity):
+        product = get_object_or_404(Product, pk=product_id)
+        self.cart.add(product, quantity)
+        return HttpResponse('ok')
