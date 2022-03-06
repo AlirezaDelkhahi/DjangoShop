e75c645847c74c8b4e2442f72f915e57b753647c
Alireza <alirezac257@gmail.com>
2022-03-06 15:59:06 +0330

    [*] fixed a bug causing of an issue for quantity

--- Shop/order/cart.py
+++ Shop/order/cart.py
@@ -4,9 +4,9 @@ CART_SESSION_ID = 'cart'
 class Cart:
     def __init__(self, request):
         self.session = request.session
-        cart = self.session.get(CART_SESSION_ID)
+        cart = self.session.get(CART_SESSION_ID, None)
         if not cart:
-            cart = self.session[CART_SESSION_ID]
+            cart = self.session[CART_SESSION_ID] = {}
         self.cart = cart
     
     def __iter__(self):
@@ -28,11 +28,15 @@ class Cart:
     def add(self, product, quantity):
         product_id = str(product.id)
 
-        if product_id not in self.cart:
+        if product_id not in self.cart.keys():
+            print('test1')
             self.cart[product_id] = {
                 'quantity': 0,
                 'price': str(product.price)
                 }
+            self.cart[product_id]['quantity'] += quantity
+            self.save()
+        else:
             self.cart[product_id]['quantity'] += quantity
             self.save()
     
@@ -41,3 +45,4 @@ class Cart:
     
     def get_total_price(self):
         return sum(Decimal(item['price'])) * item['quantity']
+
