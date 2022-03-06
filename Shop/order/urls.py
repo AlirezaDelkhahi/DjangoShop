b083f74602f071ecdaf7cbf986ce9650e75bb89e
Alireza <alirezac257@gmail.com>
2022-03-06 15:59:20 +0330

    [+] add to cart url path

--- Shop/order/urls.py
+++ Shop/order/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import CartView
 app_name = 'order'
 
 urlpatterns = [
+    path('add_to_cart/<int:product_id>/<int:quantity>', CartView.as_view(), name='cart')
 ]
