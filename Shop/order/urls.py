207d6d886817667cf6dc5e64f4520b3bd51c24e9
Alireza <alirezac257@gmail.com>
2022-03-08 18:23:33 +0330

    [+] cart url paths

--- Shop/order/urls.py
+++ Shop/order/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import CartView
+from .views import CartView, CartList, CartDetails, CartItemDelete
 app_name = 'order'
 
 urlpatterns = [
-    path('add_to_cart/<int:product_id>/<int:quantity>', CartView.as_view(), name='cart')
+    path('cart_details/' , CartDetails.as_view(), name='cart-details'),
+    path('get_cart/' , CartList.as_view(), name='cart-list'),
+    path('delete_cart_item/<int:product_id>' , CartItemDelete.as_view(), name='item-delete'),
+
+    path('add_to_cart/<int:product_id>/<int:quantity>', CartView.as_view(), name='add-to-cart')
 ]
