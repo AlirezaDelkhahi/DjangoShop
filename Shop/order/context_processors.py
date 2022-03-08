f7ed345046ac111abfcbdd6e8da448c5d3945dda
Alireza <alirezac257@gmail.com>
2022-03-08 18:23:01 +0330

    [+] open order context

--- Shop/order/context_processors.py
+++ Shop/order/context_processors.py
@@ -1,5 +1,11 @@
 from .cart import Cart
-
+from order.models import Order
 
 def cart(request):
     return {'cart': Cart(request)}
+
+def open_order(request):
+    if request.user.is_authenticated:
+        open_order = Order.objects.get(customer=request.user.customer, is_active=True)
+        return {'open_order': open_order.id}
+    return {'open_order': None}
