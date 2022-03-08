799ca47aecb32de38dcce11cd12437f1906f3bd6
Alireza <alirezac257@gmail.com>
2022-03-08 18:23:48 +0330

    [+] cart views

--- Shop/order/views.py
+++ Shop/order/views.py
@@ -1,23 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
 from .cart import Cart
 from product.models import Product
 from django.shortcuts import get_object_or_404
+from order.forms import AddressForm
 # Create your views here.
 
 
 class CartView(View):
+
+    def setup(self, request, *args, **kwargs):
+        self.cart = Cart(request)
+        return super().setup(request, *args, **kwargs)  
+
+    def post(self,request, product_id, quantity):
+        product = get_object_or_404(Product, pk=product_id)
+        self.cart.add(product, quantity)
+        return JsonResponse({'TotalItems': self.cart.__len__(), 'url':reverse('order:cart-list')}, safe=False)
+    
+    
+
+class CartList(View):
     template_name = 'order/cart.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class CartDetails(View):
+    template_name = 'order/cart_detail.html'
+
+    def get(self, request):
+        return render(request, self.template_name, { 'address_form':AddressForm(customer=request.user.customer)})  
+
+class CartItemDelete(View):
 
     def setup(self, request, *args, **kwargs):
         self.cart = Cart(request)
         return super().setup(request, *args, **kwargs)
 
-    # def get(self, request):
-    #     return render(request, template_name, {"cart": self.cart})
-
-    def post(self,request, product_id, quantity):
+    def delete(self, request, product_id):
         product = get_object_or_404(Product, pk=product_id)
-        self.cart.add(product, quantity)
-        return HttpResponse('ok')
+        self.cart.remove(product=product)
+        return JsonResponse({'TotalItems': self.cart.__len__(), 'url':reverse('order:cart-list'), 'total_price':self.cart.get_total_price()}, safe=False)
+
+
