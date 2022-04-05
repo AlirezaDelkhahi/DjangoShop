b4fefacdc84f7050c5397b396a0f7f56ed5668c0
alireza delkhahi <alirezac257@gmail.com>
2022-04-05 04:31:58 +0430

    [+] payment and offcode check views

--- Shop/order/views.py
+++ Shop/order/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.http import HttpResponse, JsonResponse
 from django.views import View
+import sweetify
+from customer.models import Address
 from .cart import Cart
 from product.models import Product
 from django.shortcuts import get_object_or_404
 from order.forms import AddressForm
-# Create your views here.
+from .models import Order, Coupon
 
 
 class CartView(View):
@@ -45,4 +48,52 @@ class CartItemDelete(View):
         self.cart.remove(product=product)
         return JsonResponse({'TotalItems': self.cart.__len__(), 'url':reverse('order:cart-list'), 'total_price':self.cart.get_total_price()}, safe=False)
 
+class Payment(View):
 
+    def setup(self, request, *args, **kwargs) -> None:
+        self.cart = Cart(request)
+        return super().setup(request, *args, **kwargs)
+
+    def post(self,request):
+        address = Address.objects.get(id=request.POST['address'])
+        open_order = Order.objects.get(customer=request.user.customer, is_active=True)
+        open_order.address = address
+        
+        open_order.deactivate()
+        
+        open_order.save()
+        
+        self.cart.clear()
+        
+        
+        Order.objects.create(customer=request.user.customer)
+        sweetify.toast(request, "Your Order Submitted Successfully.", icon="success", timer=1500)
+
+        return redirect('product:home')
+
+
+class OffCodeCheck(View):
+    def setup(self, request, *args, **kwargs):
+        offcode = request.POST.get('offCode')
+        self.offcode = Coupon.objects.filter(code = offcode, is_active = True,
+                                             valid_from__lte = timezone.now(),
+                                             valid_to__gte = timezone.now())
+        self.offcode = self.offcode.first() if self.offcode.exists() else None
+        print(self.offcode)
+        self.open_order = Order.objects.get(customer=request.user.customer, is_active=True) 
+        return super().setup(request, *args, **kwargs)
+
+
+    def post(self, request):
+        if not self.offcode:
+            return JsonResponse({'msg': 'NotValid'}, safe=False)
+        if self.open_order.coupon:
+            self.open_order.coupon = None
+            self.open_order.save()
+            self.open_order.coupon = self.offcode
+            self.open_order.save()
+        else:
+            self.open_order.coupon = self.offcode
+            self.open_order.save()
+        print(self.open_order.final_price)
+        return JsonResponse({"msg": 'Success', "final_price": self.open_order.final_price}, safe=False)
