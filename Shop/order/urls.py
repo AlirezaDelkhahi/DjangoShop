a3fca08be598007436c4cabc2eb556cf4d0d283c
alireza delkhahi <alirezac257@gmail.com>
2022-04-05 04:31:31 +0430

    [+] payment and offcode check paths

--- Shop/order/urls.py
+++ Shop/order/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import CartView, CartList, CartDetails, CartItemDelete
+from .views import CartView, CartList, CartDetails, CartItemDelete, Payment, OffCodeCheck
 app_name = 'order'
 
 urlpatterns = [
     path('cart_details/' , CartDetails.as_view(), name='cart-details'),
     path('get_cart/' , CartList.as_view(), name='cart-list'),
     path('delete_cart_item/<int:product_id>' , CartItemDelete.as_view(), name='item-delete'),
-
-    path('add_to_cart/<int:product_id>/<int:quantity>', CartView.as_view(), name='add-to-cart')
+    path('offcode_check', OffCodeCheck.as_view(), name='offcode-check'),
+    path('add_to_cart/<int:product_id>/<int:quantity>', CartView.as_view(), name='add-to-cart'),
+    path('payment/', Payment.as_view(), name='payment')
 ]
