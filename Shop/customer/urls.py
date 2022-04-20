68c5de23242be50f4259fc79d69bc830506e8596
alireza <alirezac257@gmail.com>
2022-04-20 05:43:03 +0430

    [+] dashboard urls added

--- Shop/customer/urls.py
+++ Shop/customer/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import UserRegisterView, UserLoginView, UserLogoutView, UserPasswordResetView, UserPasswordResetDoneView, \
-    UserPasswordResetConfirmView, UserPasswordResetCompleteView, PanelView
+    UserPasswordResetConfirmView, UserPasswordResetCompleteView, PanelView, UserAddressView, UserProfileView, \
+     UserProfileEditView, UserProfileImageEditView, UserAddressCreateView, UserAddressDeleteView
+    
 
 app_name = 'customer'
 
@@ -12,6 +14,12 @@ urlpatterns = [
     path('reset/done/', UserPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('confirm/<uidb64>/<token>', UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/complete', UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('panel', PanelView.as_view(), name='panel'),
+    path('panel/', PanelView.as_view(), name='panel'),
+    path('address/<pk>', UserAddressView.as_view(), name='address-edit'),
+    path('address/', UserAddressCreateView.as_view(), name='address-create'),
+    path('address-delete/<pk>', UserAddressDeleteView.as_view(), name='address-delete'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('profile/<pk>', UserProfileEditView.as_view(), name='profile-edit'),
+    path('profile-image/<pk>', UserProfileImageEditView.as_view(), name='profile-image-edit'),
     
 ]
