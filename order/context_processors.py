4db2d93ddb7ab34df4c461ca116095b2ff7c38e9
alireza <alirezac257@gmail.com>
2022-04-25 16:07:11 +0430

    [*] a bug fixed causing error for first user

--- order/context_processors.py
+++ order/context_processors.py
@@ -1,14 +1,13 @@
 from .cart import Cart
 from order.models import Order
+from customer.models import Customer
 
 def cart(request):
     return {'cart': Cart(request)}
 
 def open_order(request):
     if request.user.is_authenticated:
-        try:
-            open_order = Order.objects.get(customer=request.user.customer, is_active=True)
-        except:
-            open_order = None
+        customer, created = Customer.objects.get_or_create(user=request.user)
+        open_order, created = Order.objects.get_or_create(customer=customer, is_active=True)
         return {'open_order': open_order.id}
     return {'open_order': None}
